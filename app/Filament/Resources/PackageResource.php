c3c43ffaea6e355639ef7571aa1156926b83af72
robbybirhamn <robbybirhamn@gmail.com>
2023-06-26 19:47:03 +0700

    add package

---
+++ app/Filament/Resources/PackageResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PackageResource\Pages;
+use App\Filament\Resources\PackageResource\RelationManagers;
+use App\Models\Package;
+use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PackageResource extends Resource
+{
+    protected static ?string $model = Package::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required(),
+                TextInput::make('price')->required(),
+                Grid::make(1)
+                ->schema([
+                    RichEditor::make('description')
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('price'),
+                Tables\Columns\TextColumn::make('count_request'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPackages::route('/'),
+            'create' => Pages\CreatePackage::route('/create'),
+            'edit' => Pages\EditPackage::route('/{record}/edit'),
+        ];
+    }    
+}
